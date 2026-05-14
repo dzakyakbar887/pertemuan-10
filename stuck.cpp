4c1aa4bf2916bfb9f3c8fd2d1957385a35d9a31d
dzakyakbar887 <dzakyakbar392@gmail.com>
2026-05-14 10:46:20 +0700

    case 2

--- stuck.cpp
+++ stuck.cpp
@@ -97,4 +97,15 @@ int main()
             stack.push(value);
             break;
 
+        case 2:
+            if (!stack.isEmpty())
+            {
+                stack.pop();
+            }
+            else
+            {
+                cout << "Stack is empty. Cannot pop." << endl;
+            }
+            break;
+
         
