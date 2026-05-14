c448d66463f78ba0c581493d95c16a8880e5b695
dzakyakbar887 <dzakyakbar392@gmail.com>
2026-05-14 10:42:39 +0700

    if void peek

--- stuck.cpp
+++ stuck.cpp
@@ -49,4 +49,21 @@ public:
     }
 
     void peek()
-    
+    {
+        if (top == NULL)
+        {
+            cout << "List is empty." << endl;
+        }
+        else
+        {
+            Node *current = top;
+            while (current != NULL)
+            {
+                cout << current->data << " " << endl;
+                current = current->next;
+            }
+
+            cout << endl;
+        }
+    }
+
